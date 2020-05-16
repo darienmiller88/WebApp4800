f0f48a829538e7fbe1981dfc799677c70ceaf0fd
darienmiller88 <darienmiller88@yahoo.com>
2020-05-16 10:42:46 -0400

    front end working

--- routes.go
+++ routes.go
@@ -1,9 +1,67 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"text/template"
+
 	"github.com/gorilla/mux"
 )
 
-func createRoutes(router *mux.Router) {
+type Routes struct {
+	myRouter *mux.Router
+}
 
+//The structs that I am using must start with a capital
+type Data struct {
+	Welcome string
+	Names   []string
+}
+
+//Method to assign a new mux router to an instance of a "Routes" object and
+func (r *Routes) createRoute() {
+	r.myRouter = mux.NewRouter()
+
+	//This is the golang way of serving static files lmao
+	r.myRouter.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	//routes for web app
+	r.myRouter.HandleFunc("/", landingPage).Methods("GET")
+	r.myRouter.HandleFunc("/signup", signUp).Methods("GET")
+	r.myRouter.HandleFunc("/aboutus", aboutUs).Methods("GET")
+}
+
+func landingPage(writer http.ResponseWriter, request *http.Request) {
+	// request.FormValue("name")
+	//fmt.Fprintf(writer, "landing")
+	fmt.Println("landing page called")
+	renderTemplate(writer, request, "templates/landingPage.html", Data{
+		Welcome: "Landing",
+		Names: []string{
+			"Darien",
+			"Denise",
+			"Dalton",
+			"Derick",
+			"Doflamingo",
+		},
+	})
+}
+
+//GET request
+func signUp(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("sign up page called")
+	renderTemplate(writer, request, "templates/signup.html", nil)
+}
+
+//GET request
+func aboutUs(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("about us page called")
+	renderTemplate(writer, request, "templates/aboutUs.html", nil)
+}
+
+func renderTemplate(response http.ResponseWriter, request *http.Request, fileName string, data interface{}) {
+	myTemplate := template.Must(template.ParseFiles(fileName))
+	if err := myTemplate.Execute(response, data); err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+	}
 }
--- server.go
+++ server.go
@@ -5,77 +5,21 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"text/template"
-
-	"github.com/gorilla/mux"
 	// "encoding/json"
 	// "math/rand"
 	// "strconv"
 )
 
-type FireEmblemCharacter struct {
-	name string
-}
-
-type Waifu struct {
-}
-
 func main() {
 	//create the router that will be used to create our routes and methods
-	goRouter := mux.NewRouter()
+	routeObject := Routes{}
 
-	//This is the golang way of serving static files lmao
-	goRouter.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	//Call the function that will assign a mux router
+	routeObject.createRoute()
 
-	//routes for "api"
-	goRouter.HandleFunc("/", landingPage).Methods("GET")
-	goRouter.HandleFunc("/myapi/FireEmblemCharacters", getFireEmblemCharacters).Methods("GET")
-	goRouter.HandleFunc("/myapi/{character}", getFireEmblemCharacter).Methods("GET")
-	goRouter.HandleFunc("/myapi/addCharacter", addCharacter).Methods("POST")
-	goRouter.HandleFunc("/myapi/addWaifu", addWaifu).Methods("GET")
-	goRouter.HandleFunc("/myapi/deleteCharacter", deleteCharacter).Methods("DELETE")
-
+	//Finally, use the mux router that belongs to our route object to assign to the listen and serve function
 	fmt.Println("Server is running!")
-	log.Fatal(http.ListenAndServe(getPort("3000"), goRouter))
-}
-
-func landingPage(writer http.ResponseWriter, request *http.Request) {
-	// request.FormValue("name")
-	//fmt.Fprintf(writer, "landing")
-	renderTemplate(writer, request, "templates/landingPage.html")
-}
-
-//GET request
-func getFireEmblemCharacters(writer http.ResponseWriter, request *http.Request) {
-	renderTemplate(writer, request, "templates/feCharacters.html")
-}
-
-//GET request
-func getFireEmblemCharacter(writer http.ResponseWriter, request *http.Request) {
-
-}
-
-//POST request
-func addCharacter(writer http.ResponseWriter, request *http.Request) {
-
-}
-
-//DELETE request
-func deleteCharacter(writer http.ResponseWriter, request *http.Request) {
-
-}
-
-//PUT request
-func addWaifu(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "waifu called")
-	renderTemplate(writer, request, "templates/waifu.html")
-}
-
-func renderTemplate(response http.ResponseWriter, request *http.Request, fileName string) {
-	myTemplate := template.Must(template.ParseFiles(fileName))
-	if err := myTemplate.Execute(response, nil); err != nil {
-		http.Error(response, err.Error(), http.StatusInternalServerError)
-	}
+	log.Fatal(http.ListenAndServe(getPort("3000"), routeObject.myRouter))
 }
 
 func getPort(defaultPort string) string {
